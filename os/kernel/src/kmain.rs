00a912a2b6983f56505839511ea4038c82f6e0b5
Brent Bettis <bbettis@expedia.com>
2020-02-21 15:14:45 -0800

    merging 2-fs

--- os/kernel/src/kmain.rs
+++ os/kernel/src/kmain.rs
@@ -6,12 +6,17 @@
 #![feature(decl_macro)]
 #![feature(repr_align)]
 #![feature(attr_literals)]
-#![feature(never_type)]
-#![feature(ptr_internals)]
+#![feature(exclusive_range_pattern)]
+#![feature(alloc, allocator_api, global_allocator)]
 
+#[macro_use]
+#[allow(unused_imports)]
+extern crate alloc;
 extern crate pi;
 extern crate stack_vec;
+extern crate fat32;
 
+pub mod allocator;
 use pi::uart::MiniUart;
 use std::io::{Read, Write};
 use std::fmt::Write as OtherWrite;
@@ -20,8 +25,21 @@ pub mod lang_items;
 pub mod mutex;
 pub mod console;
 pub mod shell;
+pub mod fs;
+
+#[cfg(not(test))]
+use allocator::Allocator;
+use fs::FileSystem;
+
+#[cfg(not(test))]
+#[global_allocator]
+pub static ALLOCATOR: Allocator = Allocator::uninitialized();
+
+pub static FILE_SYSTEM: FileSystem = FileSystem::uninitialized();
 
 #[no_mangle]
+#[cfg(not(test))]
 pub extern "C" fn kmain() {
+    ALLOCATOR.initialize();
     shell::shell("> ");
 }

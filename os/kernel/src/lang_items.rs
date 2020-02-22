5fdda74612de28651e90ff40fa7c2a95ca721c1d
Brent Bettis <bbettis@expedia.com>
2020-02-21 23:52:35 -0800

    awesome panic message

--- os/kernel/src/lang_items.rs
+++ os/kernel/src/lang_items.rs
@@ -2,7 +2,28 @@
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
 pub extern fn panic_fmt(fmt: ::std::fmt::Arguments, file: &'static str, line: u32, col: u32) -> ! {
-    // FIXME: Print `fmt`, `file`, and `line` to the console.
+    use console::kprintln;
+
+    kprintln!("                             ____");
+    kprintln!("                     __,-~~/~    `---.");
+    kprintln!("                   _/_,---(      ,    )");
+    kprintln!("               __ /        <    /   )  \\___");
+    kprintln!("- ------===;;;'====------------------===;;;===----- -  -");
+    kprintln!("                  \\/  ~\"~\"~\"~\"~\"~\\~\"~)~\"/");
+    kprintln!("                  (_ (   \\  (     >    \\)");
+    kprintln!("                   \\_( _ <         >_>'");
+    kprintln!("                      ~ `-i' ::>|--\"");
+    kprintln!("                          I;|.|.|");
+    kprintln!("                         <|i::|i|`.");
+    kprintln!("                        (` ^'\"'-' \")");
+    kprintln!("-----------------------------------------------------------");
+    kprintln!("-------------------------- PANIC --------------------------");
+    kprintln!("");
+    kprintln!("FILE: {}", file);
+    kprintln!("LINE: {}", line);
+    kprintln!("COL: {}", col);
+    kprintln!("");
+    kprintln!("{}", fmt);
 
     loop { unsafe { asm!("wfe") } }
 }

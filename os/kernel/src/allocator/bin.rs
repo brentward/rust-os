b66912bc760dacf796d4e96ce6d520ba7c88d0dc
Brent Bettis <bbettis@expedia.com>
2020-02-24 01:02:47 -0800

    removing pull memory from above and dynamically getting block sizes

--- os/kernel/src/allocator/bin.rs
+++ os/kernel/src/allocator/bin.rs
@@ -1,80 +1,96 @@
-use std::fmt;
+use std::{fmt, mem, usize};
 use alloc::heap::{AllocErr, Layout};
 
 use allocator::util::*;
 use allocator::linked_list::LinkedList;
 
+const BLOCK_SIZE_COUNT: usize = mem::size_of::<usize>() * 8 - 3;
+
 /// A simple allocator that allocates based on size classes.
-#[derive(Debug)]
 pub struct Allocator {
+    total_mem: usize,
+    max_block_size: usize,
+    sized_block_count: usize,
     current: usize,
     end: usize,
-    block_sizes: [usize; 10],
-    sized_lists: [LinkedList; 10],
-    // FIXME: Add the necessary fields.
+    sized_blocks: [LinkedList; BLOCK_SIZE_COUNT],
 }
-
 impl Allocator {
     /// Creates a new bin allocator that will allocate memory from the region
     /// starting at address `start` and ending at address `end`.
     pub fn new(start: usize, end: usize) -> Allocator {
+        let total_mem = end - start;
+        let max_block_size = 1 << (mem::size_of::<usize>() * 8 - total_mem.leading_zeros() as usize - 1);
+        let sized_block_count = (max_block_size as u64).trailing_zeros() as usize - 2;
         Allocator {
+            total_mem,
+            max_block_size,
+            sized_block_count,
             current: start,
             end,
-            block_sizes: [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096],
-            sized_lists: [LinkedList::new(); 10],
+            sized_blocks: [LinkedList::new(); BLOCK_SIZE_COUNT],
         }
     }
 
-    fn get_size_index(&self, layout: &Layout) -> Option<usize> {
+    fn size_of_block(index: usize) -> usize {
+        2usize.pow(index as u32 + 3)
+    }
+
+    fn size_list_index_for_layout(&self, layout: &Layout) -> usize {
         let required_size = layout.size().max(layout.align());
-        self.block_sizes.iter().position(| &size | size >= required_size)
-    }
-
-    fn pop_from_above(&mut self, index: usize) -> Option<*mut usize> {
-        if (index + 2) <= self.block_sizes.len() {
-            if !self.sized_lists[index + 1].is_empty() {
-                return self.sized_lists[index + 1].pop()
+        for index in 0..self.sized_blocks.len() {
+            if Allocator::size_of_block(index) >= required_size{
+                return index
             }
         }
-        None
+        self.sized_blocks.len() - 1
+        // self.block_sizes.iter().position(| &size | size >= required_size)
     }
 
-    fn split_addr(addr: *mut usize, size: usize) -> (*mut usize, *mut usize) {
-        let new_addr = unsafe { *addr as usize  + size };
-        let new_ptr = new_addr as *mut usize;
-        (addr, new_ptr)
-    }
-
-    fn push_from_above(&mut self, index: usize, low_item: *mut usize, high_item: *mut usize)  {
-        unsafe {
-            self.sized_lists[index].push(low_item);
-            self.sized_lists[index].push(high_item);
-        }
-    }
-
-    fn populate_from_above(&mut self, index: usize) -> Option<()> {
-        if self.block_sizes.len() <= index {
-            return None
-        }
-        while self.sized_lists[index].is_empty() {
-            match self.populate_from_above(index + 1) {
-                Some(_) => (),
-                None => return None,
-            }
-        }
-        match self.pop_from_above(index) {
-            Some(addr) => {
-                let (low_addr, high_addr) = Allocator::split_addr(
-                    addr,
-                    self.block_sizes[index]
-                );
-                self.push_from_above(index, low_addr, high_addr);
-                Some(())
-            }
-            None => None,
-        }
-    }
+    // fn pop_from_above(&mut self, index: usize) -> Option<*mut usize> {
+    //     if (index + 2) <= self.sized_blocks.len() {
+    //         if !self.sized_blocks[index + 1].is_empty() {
+    //             return self.sized_blocks[index + 1].pop()
+    //         }
+    //     }
+    //     None
+    // }
+    //
+    // fn split_addr(addr: *mut usize, size: usize) -> (*mut usize, *mut usize) {
+    //     let new_addr = unsafe { *addr as usize  + size };
+    //     let new_ptr = new_addr as *mut usize;
+    //     (addr, new_ptr)
+    // }
+    //
+    // fn push_from_above(&mut self, index: usize, low_item: *mut usize, high_item: *mut usize)  {
+    //     unsafe {
+    //         self.sized_blocks[index].push(low_item);
+    //         self.sized_blocks[index].push(high_item);
+    //     }
+    // }
+    //
+    // fn populate_from_above(&mut self, index: usize) -> Option<()> {
+    //     if self.sized_blocks.len() <= index {
+    //         return None
+    //     }
+    //     while self.sized_blocks[index].is_empty() {
+    //         match self.populate_from_above(index + 1) {
+    //             Some(_) => (),
+    //             None => return None,
+    //         }
+    //     }
+    //     match self.pop_from_above(index) {
+    //         Some(addr) => {
+    //             let (low_addr, high_addr) = Allocator::split_addr(
+    //                 addr,
+    //                 Allocator::size_of_block(index)
+    //             );
+    //             self.push_from_above(index, low_addr, high_addr);
+    //             Some(())
+    //         }
+    //         None => None,
+    //     }
+    // }
 
 
     /// Allocates memory. Returns a pointer meeting the size and alignment
@@ -98,41 +114,49 @@ impl Allocator {
     /// (`AllocError::Exhausted`) or `layout` does not meet this allocator's
     /// size or alignment constraints (`AllocError::Unsupported`).
     pub fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
-        match self.get_size_index(&layout) {
-            Some(index) => {
-                match self.sized_lists[index].pop() {
-                    Some(addr) => Ok(addr as *mut u8),
-                    None => {
-                        match self.populate_from_above(index) {
-                            Some(_) => {
-                                match self.sized_lists[index].pop() {
-                                    Some(addr) => Ok(addr as *mut u8),
-                                    None => panic!("item in list should be guarenteed")
-                                }
-                            },
-                            None => {
-                                let aligned_addr = align_up(self.current, self.block_sizes[index]);
-                                if aligned_addr + self.block_sizes[index] > self.end {
-                                    Err(AllocErr::Exhausted { request: layout })
-                                } else {
-                                    self.current = aligned_addr + self.block_sizes[index];
-                                    Ok(aligned_addr as *mut u8)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            None => {
-                let aligned_addr = align_up(self.current, layout.align());
-                if aligned_addr + layout.size() > self.end {
-                    Err(AllocErr::Exhausted { request: layout })
-                } else {
-                    self.current = aligned_addr + layout.size();
-                    Ok(aligned_addr as *mut u8)
-                }
+        let index =  self.size_list_index_for_layout(&layout);
+        for addr in self.sized_blocks[index].iter_mut() {
+            if has_allignment(addr.value() as usize, layout.align()) {
+                return Ok(addr.pop() as *mut u8)
             }
         }
+
+        let aligned_addr = align_up(
+            self.current,
+            Allocator::size_of_block(index)
+        );
+        if aligned_addr + Allocator::size_of_block(index) > self.end {
+            Err(AllocErr::Exhausted { request: layout })
+        } else {
+            self.current = aligned_addr + Allocator::size_of_block(index);
+            Ok(aligned_addr as *mut u8)
+        }
+        //
+        // match self.sized_blocks[index].pop() {
+        //     Some(addr) => Ok(addr as *mut u8),
+        //     None => {
+        //         match self.populate_from_above(index) {
+        //             Some(_) => {
+        //                 match self.sized_blocks[index].pop() {
+        //                     Some(addr) => Ok(addr as *mut u8),
+        //                     None => panic!("item in list should be guarenteed")
+        //                 }
+        //             },
+        //             None => {
+        //                 let aligned_addr = align_up(
+        //                     self.current,
+        //                     Allocator::size_of_block(index)
+        //                 );
+        //                 if aligned_addr + Allocator::size_of_block(index) > self.end {
+        //                     Err(AllocErr::Exhausted { request: layout })
+        //                 } else {
+        //                     self.current = aligned_addr + Allocator::size_of_block(index);
+        //                     Ok(aligned_addr as *mut u8)
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
 
     /// Deallocates the memory referenced by `ptr`.
@@ -149,12 +173,33 @@ impl Allocator {
     /// Parameters not meeting these conditions may result in undefined
     /// behavior.
     pub fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
-        match self.get_size_index(&layout) {
-            Some(index) => unsafe {
-                self.sized_lists[index].push(ptr as *mut usize)
-            }
-            None => (),
+        let index = self.size_list_index_for_layout(&layout);
+        unsafe {
+            self.sized_blocks[index].push(ptr as *mut usize)
         }
+    }
+}
+
+impl fmt::Debug for Allocator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Allocator {{")?;
+        writeln!(f, "  current: {}", self.current)?;
+        writeln!(f, "  end: {}", self.end)?;
+        writeln!(f, "  total mem: {}", self.total_mem)?;
+        writeln!(f, "  max block size: {}", self.max_block_size)?;
+        writeln!(f, "  sized block count: {}", self.sized_block_count)?;
+        for i in 0..self.sized_block_count {
+            writeln!(
+                f,
+                "  bin#{} size={} = {:#?}",
+                i,
+                Allocator::size_of_block(i),
+                self.sized_blocks[i]
+            )?;
+        }
+        writeln!(f, "}}")?;
+
+        Ok(())
     }
 }
 //
